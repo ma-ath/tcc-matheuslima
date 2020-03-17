1299af3a768e583c06a5e6863742d5de1855de9f
ma-ath <m.mathelima@gmail.com>
2020-03-16 23:23:41 -0300

    feito processamento do audio

--- leomazza.py
+++ leomazza.py
@@ -5,6 +5,34 @@ from tensorflow import keras
 from keras.optimizers import Adam
 from keras.losses import mean_squared_error
 from network_model import networkModel
+import numpy as np
+from PIL import Image
+import re
+from matplotlib import pyplot as plt
+
+def loadImagesDataset():
+    imageData = []
+
+    #   Load images in alphabetical order #####
+    myimages = [] #list of image filenames
+    dirFiles = os.listdir('./dataset/images/') #list of directory files
+    dirFiles.sort(key=lambda f: int(re.sub('\D', '', f)))
+    sorted(dirFiles) #sort numerically in ascending order
+
+    for files in dirFiles: #filter out all non jpgs
+        if '.jpg' in files:
+            myimages.append(files)
+    for i in myimages:
+        im=Image.open("./dataset/images/"+i)
+        imageData.append(im)
+
+    inputData = np.zeros((len(imageData),240,240,3))
+
+    for i in range(0,len(imageData)):
+        inputData[i] = np.array(imageData[i]) # Transform from Pillow array to a Numpy array
+
+    return inputData
+
 
 image_shape = (240,240,3)           #input layer receives an RGB 240x240 image
 lr_list = [0.001, 0.0003, 9e-05]    #loss rate for the training process (Adam optimizer)
@@ -17,3 +45,9 @@ model.compile(optimizer=opt, loss=mean_squared_error)
 
 model.summary()
 
+# Import Data
+labelData = np.load('audioPower.npy')
+inputData = loadImagesDataset()
+
+plt.imshow(inputData[0], interpolation='nearest')
+plt.show()
