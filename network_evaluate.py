a3b85a5e99cfb66a8f762b1743637ae3d43d5845
ma-ath <m.mathelima@gmail.com>
2020-05-06 21:55:16 -0300

    network_evalute só carrega em memória o test data
    Former-commit-id: 4e00dcb46cf8c6012f47d089676316564e7d391a

--- network_evaluate.py
+++ network_evaluate.py
@@ -37,18 +37,16 @@ model.compile(optimizer=opt, loss=mean_squared_error)#, metrics=['accuracy'])
 model.summary()                     #Show network model
 
 [
-    X_train,
-    Y_train,
     X_test,
     Y_test
-] = loadDataset(PROCESSED_DATA_FOLDER,image_shape)                   #Load the dataset
+] = loadDataset_testOnly(PROCESSED_DATA_FOLDER,image_shape)                   #Load the dataset
 
 score = model.evaluate(X_test, Y_test, verbose=2)
 print('Model\'s score: ', score)
 
 seed()
 
-for i in range(10):
+for i in range(50):
     image_index = randrange(0,X_test.shape[0],1)
 
     X_predict = np.expand_dims(X_test[image_index],0)
