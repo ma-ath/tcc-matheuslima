ba4243af95b6a6b1ad08fcf34c2cbffcaed3fab9
ma-ath <m.mathelima@gmail.com>
2020-06-02 14:55:26 -0300

    Primeiro teste de uma convlstm

--- include/auxiliaryFunctions.py
+++ include/auxiliaryFunctions.py
@@ -12,7 +12,6 @@ from tensorflow import keras
 from keras.optimizers import Adam
 from keras.losses import mean_squared_error
 from keras.models import model_from_json
-from network_model import networkModel
 import numpy as np
 from PIL import Image
 import re
@@ -21,9 +20,19 @@ import pandas
 from random import randrange, seed
 from keras.applications.vgg16 import preprocess_input
 
-def preprocess_image(image_array):
-    mean = np.mean(image_array,axis=(0,1,2))
-    std = np.std(image_array,axis=(0,1,2))
+def preprocess_image(image_array, usecache=False,train_or_test='train'):
+    # We only need to calculate those values if we are dealing with a new dataset. So to speedup things, we can use the precalculated mean and std for train and test datasets
+    if not usecache:
+        mean = np.mean(image_array,axis=(0,1,2))
+        std = np.std(image_array,axis=(0,1,2))
+    elif train_or_test == 'train':
+        mean = [3.883008, 3.883008, 3.883008]
+        std = [31.528559, 31.528559, 31.528559]
+    elif train_or_test == 'test':
+        mean = [4.2647796, 4.2647796, 4.2647796]
+        std = [33.04215, 33.04215, 33.04215]
+    else:
+        raise ValueError
 
     image_array[:, :, :, 0] -= mean[0]
     image_array[:, :, :, 1] -= mean[1]
@@ -61,7 +70,7 @@ def preprocess_labels(label_array):
 
     return label_array, mean, std
 
-def loadDataset(PROCESSED_DATA_FOLDER,image_shape):
+def loadDataset(PROCESSED_DATA_FOLDER,image_shape, timeSteps=100,lstm=False):
     training_images = np.load(PROCESSED_DATA_FOLDER+"images_training-img.npy")
     training_labels = np.load(PROCESSED_DATA_FOLDER+"images_training-lbl.npy")
     testing_images = np.load(PROCESSED_DATA_FOLDER+"images_testing-img.npy")
@@ -88,24 +97,50 @@ def loadDataset(PROCESSED_DATA_FOLDER,image_shape):
     Y_test = np.array(Y_test).astype("float32")
 
     #Normalize the input image to have "0" mean and standart deviation of "1"
-    # We do this with the builtin keras function "preprocess_input()"
-    #I do it before loading the dataset because if done before, i would have to save the
-    #training file as float32, which takes considerably more space than a uint8 format
-    # The way I was doing it before was wrong
+    # I tried to do it using the builtin keras function "preprocess_input()", but doing it by hand has better results
+    # I do it before loading the dataset because if done before, i would have to save the
+    # training file as float32, which takes considerably more space than a uint8 format
 
-    # Mode caffe is the exact same mode used to train the vgg16 dataset
-    # Instead of this, we preprocess the inputs in a diferent way
-
-    X_train = preprocess_image(X_train)
-    X_test = preprocess_image(X_test)
+    X_train = preprocess_image(X_train,usecache=True)
+    X_test = preprocess_image(X_test,usecache=True)
 
     #This is a temporary solution, i just delete one of the audio sources
     Y_train = np.delete(Y_train, -1, axis=1)
     Y_test = np.delete(Y_test, -1, axis=1)
 
+    if lstm == False:
+        return X_train,Y_train,X_test,Y_test
+
+    number_images = X_train.shape[0]    #total number of images on dataset
+    samples = floor(number_images / timeSteps)  #number of samples for the given number of timeSteps
+    throw_away_images = number_images - samples*timeSteps   #number of images i'll have to throw away (trunc) in order to reshape the 4d tensor in the required 5d tensor
+
+    X_new_shape = (samples,timeSteps,)+image_shape    #new input format
+    Y_new_shape = (samples,timeSteps,1)               #new output format
+
+    X_train = np.delete(X_train,slice(0,throw_away_images),axis=0)  #trunc the 4d tensor
+    Y_train = np.delete(Y_train,slice(0,throw_away_images),axis=0)  #trunc the 4d tensor
+
+    X_train = np.reshape(X_train,X_new_shape)   #reshape the tensor to include the time dimension
+    Y_train = np.reshape(Y_train,Y_new_shape)   #reshape the tensor to include the time dimension
+
+    #  ---------------------------  Do same thing to test dataset   -----------------------------#
+    number_images = X_test.shape[0]    #total number of images on dataset
+    samples = floor(number_images / timeSteps)  #number of samples for the given number of timeSteps
+    throw_away_images = number_images - samples*timeSteps   #number of images i'll have to throw away (trunc) in order to reshape the 4d tensor in the required 5d tensor
+
+    X_new_shape = (samples,timeSteps,)+image_shape    #new input format
+    Y_new_shape = (samples,timeSteps,1)               #new output format
+
+    X_test = np.delete(X_test,slice(0,throw_away_images),axis=0)  #trunc the 4d tensor
+    Y_test = np.delete(Y_test,slice(0,throw_away_images),axis=0)  #trunc the 4d tensor
+
+    X_test = np.reshape(X_test,X_new_shape)   #reshape the tensor to include the time dimension
+    Y_test = np.reshape(Y_test,Y_new_shape)   #reshape the tensor to include the time dimension
+
     return X_train,Y_train,X_test,Y_test
 
-def loadDataset_testOnly(PROCESSED_DATA_FOLDER,image_shape):
+def loadDataset_testOnly(PROCESSED_DATA_FOLDER,image_shape,timeSteps=100,lstm=False):
     testing_images = np.load(PROCESSED_DATA_FOLDER+"images_testing-img.npy")
     testing_labels = np.load(PROCESSED_DATA_FOLDER+"images_testing-lbl.npy")
 
@@ -125,10 +160,27 @@ def loadDataset_testOnly(PROCESSED_DATA_FOLDER,image_shape):
     #I do it before loading the dataset because if done before, i would have to save the
     #training file as float32, which takes considerably more space than a uint8 format
 
-    X_test = preprocess_image(X_test)
+    X_test = preprocess_image(X_test,usecache=True)
 
     #Delete one of the audios channel
     Y_test = np.delete(Y_test, -1, axis=1)
+
+    if lstm == False:
+        return X_train,Y_train,X_test,Y_test
+    
+    #  ---------------------------  Do same thing to test dataset   -----------------------------#
+    number_images = X_test.shape[0]    #total number of images on dataset
+    samples = floor(number_images / timeSteps)  #number of samples for the given number of timeSteps
+    throw_away_images = number_images - samples*timeSteps   #number of images i'll have to throw away (trunc) in order to reshape the 4d tensor in the required 5d tensor
+
+    X_new_shape = (samples,timeSteps,)+image_shape    #new input format
+    Y_new_shape = (samples,timeSteps,1)               #new output format
+
+    X_test = np.delete(X_test,slice(0,throw_away_images),axis=0)  #trunc the 4d tensor
+    Y_test = np.delete(Y_test,slice(0,throw_away_images),axis=0)  #trunc the 4d tensor
+
+    X_test = np.reshape(X_test,X_new_shape)   #reshape the tensor to include the time dimension
+    Y_test = np.reshape(Y_test,Y_new_shape)   #reshape the tensor to include the time dimension
 
     return X_test,Y_test
 
@@ -137,7 +189,6 @@ def save_model(model):
     if not os.path.isdir('cache'):
         os.mkdir('cache')
     open(os.path.join('cache', 'architecture.json'), 'w').write(json_string)
-    #model.save_weights(os.path.join('cache', 'model_weights.h5'), overwrite=True)
 
 def save_weights(model):
     if not os.path.isdir('cache'):
@@ -199,3 +250,20 @@ def plotAudioPowerWithPrediction(testSamples,predictedSamples):
 
     plt.show()
     pass
+
+if __name__ == "__main__":
+    image_shape = (240,240,3)
+    PROCESSED_DATA_FOLDER = "processedData/"    #folder where all pre-processed images are located
+    timeSteps = 10
+
+    [
+    X_train,
+    Y_train,
+    X_test,
+    Y_test
+    ] = loadDataset(PROCESSED_DATA_FOLDER,image_shape,timeSteps=timeSteps,lstm=True)                   #Load the dataset
+
+    print(X_train.shape)
+    print(Y_train.shape)
+    print(X_test.shape)
+    print(Y_test.shape)
