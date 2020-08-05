5c167f76df0110af29e97f259791b4e8d825ae7d
ma-ath <m.mathelima@gmail.com>
2020-08-05 18:27:45 -0300

    rede lstm_37

--- networks.py
+++ networks.py
@@ -9,24 +9,24 @@ from include.globals_and_functions import *
     The basic network structure that is being tested is based on network a) of Carreira, J., & Zisserman, A. (2017). Quo Vadis, action recognition? A new model and the kinetics dataset.
 
     timeDistributed(VGG16) -> 
-    rcnn -> 
     timeDistributed(Pooling) -> 
-    timeDistributed(FC) -> 
-    timeDistributed(output)
+    rcnn -> 
+    Hidden FC -> 
+    FC (output)
 
     We only change some parameters
 """
 
 networks = [dict() for i in range(1)]
 
-#   Network LSTM 36 -------------------------------------------- #
-networks[0]['model_name'] = 'model_lstm_36'
+#   Network LSTM 37 -------------------------------------------- #
+networks[0]['model_name'] = 'model_lstm_37'
 networks[0]['learning_schedule'] = [0.0001, 5e-05, 1e-05]
-networks[0]['batch_size'] = 16
-networks[0]['number_of_epochs'] = 32
+networks[0]['batch_size'] = 32
+networks[0]['number_of_epochs'] = 128
 networks[0]['time_steps'] = 9
 networks[0]['features_input'] = True
-networks[0]['pooling_input'] = 'GAP'
+networks[0]['pooling_input'] = 'GMP'
 
 networks[0]['optimizer'] = 'adam'
 networks[0]['rcnn_type'] = 'lstm'
@@ -38,7 +38,7 @@ networks[0]['overlaping_window'] = True
 networks[0]['lstm_units'] = 128
 networks[0]['lstm_dropout'] = 0.2
 
-networks[0]['pooling'] = 'GAP'
+networks[0]['pooling'] = 'GMP'
 networks[0]['hidden_fc'] = True
 networks[0]['fc_nlinear_activation'] = 'tanh'
 networks[0]['fc_nlinear_size'] = 128
