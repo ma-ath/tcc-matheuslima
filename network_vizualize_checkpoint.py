28bcf4629130ee535c7ee6ea411b854678fc4dc3
ma-ath <m.mathelima@gmail.com>
2020-08-19 19:35:50 -0300

    vizualizaçao dos modelos math_nolstm
    Former-commit-id: 1d9bd7669ce647bbe8cd7cbf9c3695c798da03f7

--- network_vizualize_checkpoint.py
+++ network_vizualize_checkpoint.py
@@ -10,6 +10,24 @@ try:
     import numpy as np
     from networks import *
 
+    try:
+        X_train = np.load(PROCESSED_DATA_FOLDER+DATASET_VGG16_IMAGEFEATURES_FILEPATH+DATASET_VGG16_IMAGEFEATURES_FTRAIN)
+        Y_train = np.load(PROCESSED_DATA_FOLDER+"images_training-lbl.npy")
+        X_test = np.load(PROCESSED_DATA_FOLDER+DATASET_VGG16_IMAGEFEATURES_FILEPATH+DATASET_VGG16_IMAGEFEATURES_FTEST)
+        Y_test = np.load(PROCESSED_DATA_FOLDER+"images_testing-lbl.npy")
+
+        Y_train = np.delete(Y_train, -1, axis=1)
+        Y_test = np.delete(Y_test, -1, axis=1)
+
+        #throw_away_images = X_train.shape[0] - Y_train.shape[0]
+        #X_train = np.delete(X_train,slice(0,throw_away_images),axis=0)
+
+        #throw_away_images = X_test.shape[0] - Y_test.shape[0]
+        #X_test = np.delete(X_test,slice(0,throw_away_images),axis=0)
+    except:
+        telegramSendMessage('Error loading dataset')
+        raise
+
     #telegramSendMessage('loading dataset')
 
     #[
@@ -21,56 +39,34 @@ try:
 
     #telegramSendMessage('dataset load')
 
-    MODELOS = [ 'model_math_lstm_0',
-                'model_math_lstm_1',
-                'model_math_lstm_2',
-                'model_math_lstm_3',
-                'model_math_lstm_4',
-                'model_math_lstm_5',
-                'model_math_lstm_6',
-                'model_math_lstm_7',
-                'model_math_lstm_8',
-                'model_math_lstm_9',
-                'model_math_lstm_10',
-                'model_math_lstm_11',
-                'model_math_lstm_12',
-                'model_math_lstm_13',
-                'model_math_lstm_14',
-                'model_math_lstm_15',
-                'model_math_lstm_16',
-                'model_math_lstm_17',
-                'model_math_lstm_18',
-                'model_math_lstm_19',
-                'model_math_lstm_20',
-                'model_math_lstm_21',
-                'model_math_lstm_22',
-                'model_math_lstm_23',
-                'model_math_lstm_24',
-                'model_math_lstm_25',
-                'model_math_lstm_26',
-                'model_math_lstm_27',
-                'model_math_lstm_28',
-                'model_math_lstm_29',
-                'model_math_lstm_30',
-                'model_math_lstm_31'
+    MODELOS = [ 'model_math_nolstm_0',
+                'model_math_nolstm_1',
+                'model_math_nolstm_2',
+                'model_math_nolstm_3',
+                'model_math_nolstm_4',
+                'model_math_nolstm_5',
+                'model_math_nolstm_6',
+                'model_math_nolstm_7',
+                'model_math_nolstm_8',
+                'model_math_nolstm_9'
         ]
     
     iteracao = 0
 
     for MODELO in MODELOS:
 
-        telegramSendMessage('Loading dataset for model '+networks[iteracao]['model_name'])
-
-        [
-        X_train, 
-        Y_train,
-        X_test,
-        Y_test
-        ] = loadDatasetLSTM(causal_prediction=networks[iteracao]['dataset_causal_prediction'],
-                        overlap_windows=networks[iteracao]['dataset_overlap_windows'],
-                        timeSteps=networks[iteracao]['time_steps'],
-                        features_only=networks[iteracao]['features_input'],
-                        pooling_input=networks[iteracao]['pooling_input'])
+        #telegramSendMessage('Loading dataset for model '+networks[iteracao]['model_name'])
+        
+        #[
+        #X_train, 
+        #Y_train,
+        #X_test,
+        #Y_test
+        #] = loadDatasetLSTM(causal_prediction=networks[iteracao]['dataset_causal_prediction'],
+        #                overlap_windows=networks[iteracao]['dataset_overlap_windows'],
+        #                timeSteps=networks[iteracao]['time_steps'],
+        #                features_only=networks[iteracao]['features_input'],
+        #                pooling_input=networks[iteracao]['pooling_input'])
 
         telegramSendMessage('Starting training process for '+networks[iteracao]['model_name'])
 
@@ -115,10 +111,11 @@ try:
         plotAudioPowerWithPrediction(Y_vtest,Y_predicted,to_file=True,image_path='cache/'+MODELO,image_name='/prediction_val_checkpoint.png')
 
         # ------------------- predicte over train set ------------------- #
+        """
         Y_predicted = []
         Y_vtest = Y_train
 
-        # Prepare a predictionSamples vector, in order to plot it
+         Prepare a predictionSamples vector, in order to plot it
         for i in range(X_train.shape[0]):
             X_predict = np.expand_dims(X_train[i],0)
 
@@ -141,7 +138,7 @@ try:
         Y_vtest = np.reshape(Y_train,newshape)
 
         plotAudioPowerWithPrediction(Y_vtest,Y_predicted,to_file=True,image_path='cache/'+MODELO,image_name='/prediction_train_checkpoint.png')
-
+        """
     telegramSendMessage('script end')
 except Exception as e:
 
